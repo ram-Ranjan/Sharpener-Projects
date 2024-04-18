9c6d72f77a1f7be15fcfa28477cdcd33f0a1ebc0
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-04-18 14:41:34 +0530

    Dom manipulations based on id

--- Dom_Manipulaions/dom.js
+++ Dom_Manipulaions/dom.js
@@ -1,8 +1,30 @@
-const body = document.body;
 
-body.append("Hello World!!")
 
-const div =body.createElement("div")
-body.append(div)
+// Write your code below:
+// const body = document.body;
 
-div.innerText="Hello There";
+// body.append("Hello World!!")
+
+// const div =body.createElement("div")
+// body.append(div)
+
+// div.innerText="Hello There";
+
+let mainHead= document.getElementById("main-heading");
+mainHead.textContent="Fruit World"
+
+mainHead.style.color="orange"
+
+let head=document.getElementById("header");
+head.style.backgroundColor="green"
+head.style.borderBottom="solid 2px green"
+
+let basketHead=document.getElementById("basket-heading");
+basketHead.style.color="green"
+
+
+//let newElem = `<div id="thanks"><p>Please visit us again</p><div>`
+let thanks=document.getElementById("thanks");
+thanks.innerHTML=`<p>Please visit us again</p>`;
+
+
